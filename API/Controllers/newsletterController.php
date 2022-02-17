b7b2064659539a2d86ec87d124af91364301bec3
Victor Barraza <victor.barrazamerino@medieinstitutet.se>
2022-02-17 10:31:30 +0100

    struktur

---
+++ API/Controllers/newsletterController.php
@@ -0,0 +1,10 @@
+<?php
+
+    include_once("./../Classes/createInstanceFunctions.php");
+    include_once("./../Controllers/mainController.php");
+
+    class NewsletterController extends MainController {
+
+    }
+
+?>
